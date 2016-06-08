853048ec93266d736d37a84eefde0fe2e1837aeb
ROSA <gojiradam@gmail.com>
2016-06-08 15:05:51 +0300

    §

--- logParserMysql.py
+++ logParserMysql.py
@@ -4,9 +4,8 @@ conn = MySQLdb.connect("localhost","root","root","PRO_STORE" )
 c = conn.cursor()
 csv_data=csv.reader(file("var/log/store.log"))
 for row in csv_data:
-    c.execute("UPDATE STORE_DATA SET DATE_TIME = %s", row)
+    c.execute("INSERT INTO STORE_DATA SET DATE_TIME = %s", row)
     print row
-    #c.execute("INSERT INTO STORE_DATA (DATE_TIME, PRODUCT) VALUES ('%s', '%s')" % tuple(row))
 
 conn.commit()
 c.close()
