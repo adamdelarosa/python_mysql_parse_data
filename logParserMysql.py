be3b7d3b40da553ff5f9dba835c35423b8be22a2
ROSA <gojiradam@gmail.com>
2016-06-08 12:35:18 +0300

    §

--- logParserMysql.py
+++ logParserMysql.py
@@ -13,11 +13,10 @@ cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 
 # Create table as per requirement
 sql = """CREATE TABLE EMPLOYEE (
-         FIRST_NAME  CHAR(20) NOT NULL,
-         LAST_NAME  CHAR(20),
-         AGE INT,
-         SEX CHAR(1),
-         INCOME FLOAT )"""
+         DATE_TIME  CHAR(20) NOT NULL,
+         PRODUCT  CHAR(20),
+         KIND CHAR(20),
+         VALUE CHAR(1))"""
 
 cursor.execute(sql)
 
