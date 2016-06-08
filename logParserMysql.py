3bc4e35e86da190ba97eda3464094ff7363cddfe
ROSA <gojiradam@gmail.com>
2016-06-08 13:41:27 +0300

    §

--- logParserMysql.py
+++ logParserMysql.py
@@ -5,4 +5,21 @@ import MySQLdb
 print 'Data parser . . . '
 
 # Open database connection
-db = MySQLdb.connect("localhost","root","root","PRO_STORE" )
+db = MySQLdb.connect("localhost","root","root","STORE_DATA" )
+
+cursor = db.cursor()
+
+file = open('var/log/store.log', 'r')
+
+file_content = file.read()
+print file_content
+file.close()
+
+query = "INSERT INTO STORE_DATA VALUES (%s)"
+
+cursor.execute(query, (file_content,))
+
+print file_content
+
+db.commit()
+db.close()
