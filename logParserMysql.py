144ca203e4719dafe7879bd647d71bed1c650548
ROSA <gojiradam@gmail.com>
2016-06-08 13:09:54 +0300

    §

--- logParserMysql.py
+++ logParserMysql.py
@@ -3,21 +3,4 @@
 import MySQLdb
 
 # Open database connection
-db = MySQLdb.connect("localhost","root","root","store_data" )
-
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-
-# Drop table if it already exist using execute() method.
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-
-# Create table as per requirement
-sql = """CREATE TABLE EMPLOYEE (
-         DATE_TIME  CHAR(20) NOT NULL,
-         PRODUCT  CHAR(20),
-         KIND CHAR(20),
-         VALUE CHAR(1))"""
-
-cursor.execute(sql)
-
-db.close()
+db = MySQLdb.connect("localhost","root","root","PRO_STORE" )
